f295b1fc8d56aeb66263c7e490d540880886f757
Masahiro Honma <hiratara@cpan.org>
2019-01-04 17:18:47 +0900

    feat: support add_method and calls

--- lib/Test/LazyMock.pm
+++ lib/Test/LazyMock.pm
@@ -2,10 +2,70 @@ package Test::LazyMock;
 use 5.008001;
 use strict;
 use warnings;
+use Scalar::Util qw(weaken);
 
 our $VERSION = "0.01";
 
+sub new {
+    my $class = shift;
+    my %params = @_;
 
+    bless {
+        # TODO: support nested method defs
+        _lazymock_methods => $params{methods},
+        _lazymock_name => $params{name},
+        _lazymock_parent => $params{parent},
+        _lazymock_calls => [],
+    } => $class;
+}
+
+sub lazymock_add_method {
+    my ($self, $name, $code_or_value) = @_;
+
+    my $code;
+    if (ref $code_or_value // '' eq 'CODE') {
+        $code = $code_or_value;
+    } else {
+        $code = sub { $code_or_value };
+    }
+
+    # TODO: support nested method defs
+    $self->{_lazymock_methods}{$name} = $code;
+}
+
+sub lazymock_calls { @{$_[0]->{_lazymock_calls}} }
+
+sub DESTROY {}
+
+sub AUTOLOAD {
+    my ($self, @params) = @_;
+    (my $meth = our $AUTOLOAD) =~ s/.*:://;
+
+    # follow up the chain of mocks and record calls
+    my %seen;
+    my $cur_call = [$meth, \@params];
+    my $cur_mock = $self;
+    while ($cur_mock && ! $seen{int($cur_mock)}++) {
+        push @{$cur_mock->{_lazymock_calls}}, $cur_call;
+
+        $cur_call = [
+            join('->', $cur_mock->{_lazymock_name} // '', $cur_call->[0]),
+            $cur_call->[1],
+        ];
+        $cur_mock = $cur_mock->{_lazymock_parent};
+    }
+
+    # return value
+    my $code = $self->{_lazymock_methods}{$meth} //= do {
+        weaken(my $weaken_self = $self);
+        my $new_mock = ref($self)->new(
+            name => $meth,
+            parent => $weaken_self,
+        );
+        sub { $new_mock };
+    };
+    $code->(@params);
+}
 
 1;
 __END__
---
+++ t/basic.t
@@ -0,0 +1,34 @@
+use strict;
+use warnings;
+use Test::More import => [qw(is is_deeply done_testing)];
+use Test::LazyMock;
+
+my $mock = Test::LazyMock->new;
+# mock.hoge.return_value = 10
+$mock->lazymock_add_method(hoge => 10);
+# mock.foo.side_effect = lambda x: x + 1
+$mock->lazymock_add_method(foo => sub { $_[0] + 1 });
+
+# call any methods
+$mock->abc;
+$mock->def->ghi;
+
+# call defined methods
+is $mock->hoge, 10;
+is $mock->foo(100), 101;
+
+# assert results
+my @calls = $mock->lazymock_calls;
+is @calls, 5;
+is_deeply $calls[0], ['abc', []];
+is_deeply $calls[1], ['def', []];
+is_deeply $calls[2], ['def->ghi', []];
+is_deeply $calls[3], ['hoge', []];
+is_deeply $calls[4], ['foo', [100]];
+
+# assert sub results
+my @def_calls = $mock->def->lazymock_calls;
+is @def_calls, 1;
+is_deeply $def_calls[0], ['ghi', []];
+
+done_testing;
