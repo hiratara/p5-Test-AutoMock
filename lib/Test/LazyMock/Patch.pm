e42e1b01e8b1a388797afebd5851e643009fddc1
Masahiro Honma <hiratara@cpan.org>
2019-01-22 23:18:46 +0900

    feat: support multiple patches

--- lib/Test/LazyMock/Patch.pm
+++ lib/Test/LazyMock/Patch.pm
@@ -6,16 +6,25 @@ use Test::LazyMock::Overloaded;
 
 our @EXPORT_OK = qw(patch_sub);
 
-sub patch_sub {
-    my ($subroutine, $code) = @_;
+sub _patch_sub_one {
+    my ($code, $subroutines, $mocks) = @_;
+    my ($subroutine, @left_subroutines) = @$subroutines;
 
     my $mock = Test::LazyMock::Overloaded->new;
-    {
-        no strict 'refs';
-        no warnings 'redefine';
-        local *$subroutine = sub { $mock };
-        $code->($mock);
-    }
+    my @new_mocks = (@$mocks, $mock);
+
+    no strict 'refs';
+    no warnings 'redefine';
+    local *$subroutine = sub { $mock };
+
+    @left_subroutines
+        ? _patch_sub_one($code, \@left_subroutines, \@new_mocks)
+        : $code->(@new_mocks);
+}
+
+sub patch_sub (&@) {
+    my ($code, @subroutines) = @_;
+    _patch_sub_one $code, \@subroutines, [];
 }
 
 1;
