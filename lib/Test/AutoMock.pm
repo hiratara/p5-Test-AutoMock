2e86078cf10f4b9186796edc2f87c972227a0889
Masahiro Honma <hiratara@cpan.org>
2019-02-01 17:33:24 +0900

    docs: add pods

--- lib/Test/AutoMock.pm
+++ lib/Test/AutoMock.pm
@@ -230,15 +230,162 @@ __END__
 
 =head1 NAME
 
-Test::AutoMock - It's new $module
+Test::AutoMock - A mock that can be used with a minimum setup
 
 =head1 SYNOPSIS
 
     use Test::AutoMock;
+    use Test::More;
+
+    # a black box function you want to test
+    sub get_metacpan {
+        my $ua = shift;
+        my $response = $ua->get('https://metacpan.org/');
+        if ($response->is_success) {
+            return $response->decoded_content;  # or whatever
+        }
+        else {
+            die $response->status_line;
+        }
+    }
+
+    # build and set up the mock
+    my $mock_ua = Test::AutoMock->new(
+        methods => {
+            # implement only the method you are interested in
+            'get->decoded_content' => "Hello, metacpan!\n",
+        },
+    );
+
+    # action first
+    my $body = get_metacpan($mock_ua);
+
+    # then, assertion
+    is $body, "Hello, metacpan!\n";
+    $mock_ua->automock_called_with_ok('get->is_success' => []);
+    $mock_ua->automock_not_called_ok('get->status_line');
+
+    # print all recorded calls
+    for ($mock_ua->automock_calls) {
+        my ($method, $args) = @$_;
+        note "$method(" . join(', ', @$args) . ")";
+    }
 
 =head1 DESCRIPTION
 
-Test::AutoMock is ...
+Test::AutoMock is a mock module designed to be used with a minimal setup.
+AutoMock can respond to any method call and returns a new AutoMock instance
+as a return value. Therefore, you can use it as a mock object without having
+to define all the methods. Even if method calls are nested, there is no
+problem.
+
+Auto records all method calls on all descendants. You can verify the method
+calls and its arguments after using the mock. This is not the "record and
+replay" model but the "action and assertion" model.
+
+You can also mock many overloaded operators and hashes, arrays with
+L<Test::AutoMock::Overloaded>. If you want to apply monkey patch to use
+AutoMock, check L<Test::AutoMock::Patch>.
+
+Test::AutoMock is inspired by Python3's unittest.mock module.
+
+=head1 METHODS
+
+=head2 new
+
+    my $mock = Test::AutoMock->new(
+        methods => {
+            agent => 'libwww-perl/AutoMock',
+            'get->is_success' => sub { 1 },
+        },
+        isa => 'LWP::UserAgent',
+    );
+
+Constructor of AutoMock. It takes the following parameters.
+
+=over 4
+
+=item methods
+
+A hash-ref of method definitions. See L<automock_add_method>.
+
+=item isa
+
+A super class of this mock. See L<automock_isa>.
+To specify multiple classes, use array-ref.
+
+=back
+
+=head2 automock_add_method
+
+    $mock->automock_add_method(add_one => sub { $_[0] + 1 });
+    $mock->automock_add_method('path->to->some_obj->name' => 'some_obj');
+
+Define the behavior of AutoMock when calling a method.
+
+The first argument is the method name. You can also specify nested names with
+C<< -> >>. A call in the middle of a method chain is regarded as a field and
+can not be defined as a method at the same time. For example, if you try to
+specify C<< 'get_object->name' >> and C<'get_object'> as the same mock,
+you'll get an error.
+
+The second argument specifies the return value when the method is called.
+If you specify a code reference, that code will be called on method invocation.
+Be aware that C<$self> is not included in the argument.
+
+=head2 automock_isa
+
+    $mock->automock_isa('Foo', 'Hoge');
+
+Specify the superclass of the mock. This specification only affects the C<isa>
+method. It is convenient when argument is checked like L<Moose> field.
+
+=head2 automock_child
+
+    # return the $mock->some_field
+    $mock->automock_child('some_field');
+
+Return the mock's child. Since this call is not recorded, it is convenient when
+you want to avoid recording unnecessary calls when writing assertions.
+
+TODO: Support C<< -> >> notations.
+
+=head2 automock_calls
+
+    my @calls = $mock->automock_calls;
+
+Returns all recorded method calls. The element of "calls" is a two-element
+array-ref. The first element is a method name, and the second element is an
+array-ref representing arguments.
+
+Method calls to children are also recorded in C<$mock>. For example, calling
+C<< $mock->child->do_it >> will record two calls C<'child'> and
+C<< 'child->do_it' >>.
+
+=head2 automock_reset
+
+Erase all recorded method calls. Delete all method call history from descendant
+mocks as well. It is used when you want to reuse mock.
+
+=head2 automock_called_ok
+
+    $mock->automock_called_ok('hoge->bar');
+
+Checks if the method was called. It is supposed to be used with L<Test::More> .
+
+=head2 automock_called_with_ok
+
+    $mock->automock_called_with_ok(
+        'hoge->bar', [10, 20],
+    );
+
+Checks if the method was called with specified arguments.
+
+=head2 automock_not_called_ok
+
+    $mock->automock_not_called_ok('hoge->bar');
+
+Checks if the method was not called.
 
 =head1 LICENSE
 
